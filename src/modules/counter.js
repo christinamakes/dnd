63b5c45899e14b3e072f638ef31f66c3dab6f95a
christina.moore <christina.moore@christina-moore-mbp.local>
2018-09-06 11:56:05 -0500

    clean up

--- src/modules/counter.js
+++ src/modules/counter.js
@@ -1,12 +1,7 @@
 export const INCREMENT_REQUESTED = 'counter/INCREMENT_REQUESTED'
-export const INCREMENT = 'counter/INCREMENT'
-export const DECREMENT_REQUESTED = 'counter/DECREMENT_REQUESTED'
-export const DECREMENT = 'counter/DECREMENT'
 
 const initialState = {
   count: 0,
-  isIncrementing: false,
-  isDecrementing: false
 }
 
 export default (state = initialState, action) => {
@@ -15,26 +10,6 @@ export default (state = initialState, action) => {
       return {
         ...state,
         isIncrementing: true
-      }
-
-    case INCREMENT:
-      return {
-        ...state,
-        count: state.count + 1,
-        isIncrementing: !state.isIncrementing
-      }
-
-    case DECREMENT_REQUESTED:
-      return {
-        ...state,
-        isDecrementing: true
-      }
-
-    case DECREMENT:
-      return {
-        ...state,
-        count: state.count - 1,
-        isDecrementing: !state.isDecrementing
       }
 
     default:
@@ -51,45 +26,5 @@ export const increment = () => {
     dispatch({
       type: INCREMENT
     })
-  }
-}
-
-export const incrementAsync = () => {
-  return dispatch => {
-    dispatch({
-      type: INCREMENT_REQUESTED
-    })
-
-    return setTimeout(() => {
-      dispatch({
-        type: INCREMENT
-      })
-    }, 3000)
-  }
-}
-
-export const decrement = () => {
-  return dispatch => {
-    dispatch({
-      type: DECREMENT_REQUESTED
-    })
-
-    dispatch({
-      type: DECREMENT
-    })
-  }
-}
-
-export const decrementAsync = () => {
-  return dispatch => {
-    dispatch({
-      type: DECREMENT_REQUESTED
-    })
-
-    return setTimeout(() => {
-      dispatch({
-        type: DECREMENT
-      })
-    }, 3000)
   }
 }
